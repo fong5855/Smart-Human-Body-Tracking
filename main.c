b5f5b6c913d961545b9bb212903abfe32cbdd98e
fong <dfl@vlsilab.ee.ncku.edu.tw>
2018-05-18 03:33:43 +0000

    update readme

--- main.c
+++ main.c
@@ -16,20 +16,10 @@
 char esp_data_cmd[] = "AT+CIPSEND=0,252\r\n";
 static char http_get[] = "GET /";
 static char http_IDP[] = "+IPD,";
-/* static char http_html_header[100] = ""; */
-static char http_html_header[60] = "HTTP/1.x 200 OK\r\nContent-type: text/html\r\n\r\n";
-/* static char http_html_header[] = "HTTP/1.x 200 OK\r\nContent-type: application/octet-stream\r\n\r\n"; */
-/* static char http_html_header[] = "HTTP/1.x 200 OK\r\nContent-type: txt/plain\r\n\r\n"; */
-/* static char http_html_body_1[100] = "22"; */
-/** static char send_buf[420] = {0}; */
 
-/* extern float* pose; */
-
-char http_sent[80];
 
 int main(void)
 {
-    EMBARC_PRINTF("http_header size = %d\r\n", sizeof(http_html_header));
     char *conn_buf;
     char scan_result[1024];
     // set pin
@@ -55,13 +45,10 @@ int main(void)
     int f;
     struct vlsi_esp *vlsi_esp8266;
     vlsi_esp8266_init(vlsi_esp8266, 115200);
-    /* vlsi_esp8266_init(vlsi_esp8266, UART_BAUDRATE_230400); */
-    /* vlsi_esp8266_set_baud(vlsi_esp8266, 230400); */
     f = vlsi_esp8266_test(vlsi_esp8266);
     EMBARC_PRINTF("test %d \r\n", f);
     board_delay_ms(1000, 1);
 
-    /* vlsi_esp8266_set_baud(vlsi_esp8266, 0); */
     
     f = vlsi_esp8266_mode_set(vlsi_esp8266);
     EMBARC_PRINTF("mode set %d \r\n", f);
@@ -83,51 +70,31 @@ int main(void)
     char idp;
 
     EMBARC_PRINTF("WiFi init done... \r\n");
+
+    // initialize recieve buffer
     scan_result[0] = '\0';
 
     char* send_buf = R;
     while(1)
     {
-      /* for (int i = 0; i < 4; ++i) { */
-        /* for (int j = 0; j < 4; ++j) { */
-          /* EMBARC_PRINTF("%d ", (int)(R[i*4+j]*1000)); */
-          /* if (j == 4) { */
-            /* EMBARC_PRINTF("\r\n"); */
-          /* } */
-        /* } */
-          /* EMBARC_PRINTF("\r\n"); */
-      /* } */
-      /* EMBARC_PRINTF("-----------\r\n"); */
-
-      /* scan_result[0] = '\0'; */
       vlsi_esp8266_read(vlsi_esp8266, scan_result, 1000);
-      /* if (strstr(scan_result, http_get) != NULL) { */
+
+      // if have a call, start the trainsition
       while (scan_result[0] != 0) {
         EMBARC_PRINTF("recieve = %s ", scan_result);
+
         // find who request
         idp = *(strstr(scan_result, http_IDP)+5);
         EMBARC_PRINTF("idp = %c \r\n", idp);
 
-        /* vlsi_esp8266_send(vlsi_esp8266, http_html_header, idp); */
-        /* board_delay_ms(100, 1); */
         for (int i = 0; i < 1; ++i) {
           vlsi_esp8266_send_datas(vlsi_esp8266, esp_data_cmd, sizeof(esp_data_cmd), send_buf, 252);
-          /* vlsi_esp8266_send_pose(vlsi_esp8266, (send_buf+210*0), idp); */
           board_delay_ms(5, 1);
-          /* vlsi_esp8266_send_pose(vlsi_esp8266, (send_buf+210*1), idp); */
-          /* board_delay_ms(10, 1); */
-          /* vlsi_esp8266_send_pose(vlsi_esp8266, (send_buf+105*2), idp); */
-          /* board_delay_ms(10, 1); */
-          /* vlsi_esp8266_send_pose(vlsi_esp8266, (send_buf+105*3), idp); */
-          /* board_delay_ms(10, 1); */
         }
 
         /* vlsi_esp8266_close(vlsi_esp8266, idp); */
 
         EMBARC_PRINTF("send complete %c \r\n");
-        /* for (int i = 0; i < 1024; ++i) { */
-          /* scan_result[i] = 0; */
-        /* } */
       }
       board_delay_ms(100, 1);
     }
--- readme.md
+++ readme.md
@@ -3,7 +3,6 @@
 ## Introduction
 
 ## Required Hardware
-
 * DesignWare ARC EM Starter Kit(EMSK)
 * ESP8266 WiFi module
 * FTDI FT2232 usb to UART module
@@ -19,7 +18,7 @@
 * Client display
 
 ## Hardware Connection
-![Connection](https://imgur.com/a/rZ39DnC)  
+![Connection](imgs/arc_connection.jpg)  
 
 ## Architecture
 The communication system is driven by EMSK ARC, which is a low power always on processor, waiting until one make a call and then power up the depth camera system.  
@@ -27,7 +26,7 @@ The communication system is driven by EMSK ARC, which is a low power always on p
 We can get human skeleton by **depth camera module**. (In our case we use kinect and PC and get skeleton by OpenNI skeleton algorithm.)  
 Then we using **ROS** to integrate to environments (camera informations, skeleton informations and ftdi transition) and sent the detected skeleton to ARC through **FTDI FT2232** chip.  
 
-![](http://wiki.ros.org/openni_tracker?action=AttachFile&do=get&target=nite.png)  
+![](imgs/tracking.png)
 
 The **EMSK ARC**, which serve as the low power server, collect the detected skeleton and process the skeleton nodes let the client can easliy handle these informations. The connection between the client and ARC is the socket portocal and ARC will send the infomations to client by **ESP8266 WiFi module**.  
 
@@ -35,6 +34,7 @@ The client (At least PC, we use extra device to make more usage scenarios) recie
 
 # User Manual
 1. Start the EMSK ARC to wait for call.
+![init](imgs/arc_init.PNG)
 2. User make a call to EMSK, then the EMSK ARC will poewer up the depth camera module and sent the detect human body infomations back.
 3. The user can interact with the pose infomations.
 
